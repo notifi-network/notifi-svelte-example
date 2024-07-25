6da9a1e1e1e6a5f6c4003386c56f962b545a7246
Eric Lee <macbookpro4eric@Mac-mini.local>
2024-07-25 16:32:19 +0900

    fix: workaround for 500 internal error (svelte-preproccess-react)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script>
-  import YouTube from "react-youtube";
+  import DefaultExport from "react-youtube";
+  let YouTube = DefaultExport.default || DefaultExport  
+  /* Client imports dist/YouTube.esm.js but the Server imports dist/YouTube.js from react-youtube. above ⬆️ fix is to make sure this wont break server code.
+   * Alternatively, we can disable SSR in this case, see the detail: https://github.com/bfanger/svelte-preprocess-react/issues/40#issuecomment-2223535203
+  */
+
 </script>
 <react:YouTube videoId="AdNJ3fydeao" />
 <h1>Welcome to SvelteKit</h1>
