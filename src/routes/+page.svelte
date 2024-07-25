b066dcfd65b3ab65796079299b37f53db20c0b36
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2024-07-25 16:54:58 +0900

    MVP-5232: Bootstrap project with necessary lib (#2)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script>
+  import DefaultExport from "react-youtube";
+  let YouTube = DefaultExport.default || DefaultExport  
+  /* Client imports dist/YouTube.esm.js but the Server imports dist/YouTube.js from react-youtube. above ⬆️ fix is to make sure this wont break server code.
+   * Alternatively, we can disable SSR in this case, see the detail: https://github.com/bfanger/svelte-preprocess-react/issues/40#issuecomment-2223535203
+  */
+
+</script>
+<react:YouTube videoId="AdNJ3fydeao" />
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
