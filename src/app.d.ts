7fbf51ab60511ad834e136e7fbacbfa6f5f82a06
Eric Lee <macbookpro4eric@Mac-mini.local>
2024-07-29 18:51:59 +0900

    MVP-5233-2: Add dummy notifi-react-card using svelte-preprocess-react module

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,8 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import { Buffer as BufferPolyfill } from "buffer";
 declare global {
+  interface Buffer extends BufferPolyfill {}
   interface Window {
     ethereum: any; // TODO: Define the type (Could use ether.js). Use any for now for now for simplicity
   }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,15 @@
 <script>
   import DefaultExport from "react-youtube";
+  import {
+  NotifiContextProvider,
+  NotifiCardModal,
+  } from "@notifi-network/notifi-react";
+  import "@notifi-network/notifi-react/dist/index.css";
   import { MetaMaskStore } from "$lib";
   import { onMount } from "svelte";
   // @ts-ignore
-  let YouTube = DefaultExport.default || DefaultExport  
-  /* Client imports dist/YouTube.esm.js but the Server imports dist/YouTube.js from react-youtube. above ⬆ fix is to make sure this wont break server code.
+  let YouTube = DefaultExport.default || DefaultExport; 
+  /* Client imports dist/YouTube.esm.js but the Server imports dist/YouTube.js from react-youtube. above ⬆️ fix is to make sure this wont break server code.
    * Alternatively, we can disable SSR in this case, see the detail: https://github.com/bfanger/svelte-preprocess-react/issues/40#issuecomment-2223535203
   */
   const { walletState, isMetaMaskPresent, connect, loaded, init, signArbitrary } =
@@ -15,7 +20,6 @@
   });
 </script>
 <react:YouTube videoId="AdNJ3fydeao" />
-
 {#if $loaded}
   {#if $isMetaMaskPresent}
     {#if Boolean($walletState.account)}
@@ -30,5 +34,15 @@
 {:else}
   <p>Loading...</p>
 {/if}
+<react:NotifiContextProvider notifiContextProvider
+    tenantId="4zfoga0vjqh90ahg8apd"
+    env="Production"
+    walletBlockchain="ETHEREUM"
+    walletPublicKey="0xc8451882B5ec5A532cDcA3E07A311274f6C897ad"
+    signMessage={() => new Uint8Array([])}
+    cardId="90f9ac3f674a4a79955204afc1142a39"
+  >
+  <react:NotifiCardModal darkMode />
+</react:NotifiContextProvider>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
