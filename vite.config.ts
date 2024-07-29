7a6a4633b4d1f44d377f2f9233d4755d9a62c3b9
eric-notifi <127958634+eric-notifi@users.noreply.github.com>
2024-07-30 08:48:06 +0900

    MVP-5233-2: Add dummy notifi-react-card using svelte-preprocess-react module (#4)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,28 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import GlobalPolyFill from "@esbuild-plugins/node-globals-polyfill";
+import { resolve } from "path";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+      plugins: [
+        // @ts-ignore
+        GlobalPolyFill.default({
+          buffer: true,
+        }),
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      "@toruslabs/openlogin": resolve(
+        "./node_modules/@toruslabs/openlogin/dist/openlogin.umd.min.js"
+      ),
+    },
+  },
 });
